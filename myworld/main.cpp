c40d10cc1365056fc86be0d03256b594399b3e4f
Reem <macbookpro@macbooks-mbp.home>
2017-01-18 22:04:11 +0200

    Initial Commit

---
+++ myworld/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  myworld
+//
+//  Created by MacBook Pro on 1/18/17.
+//  Copyright © 2017 the Goodies___. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
